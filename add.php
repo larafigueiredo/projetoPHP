a0cd78319f0d45c56066a60afedb0ef350701052
larafigueiredo <66358164+larafigueiredo@users.noreply.github.com>
2022-04-20 18:08:49 +0100

    Update add.php
    update

--- add.php
+++ add.php
@@ -4,6 +4,22 @@
 </head>
 
 <body>
+
+<style>
+html, body {
+	margin: 5px;
+	padding: 5px;
+	border: 0;
+	font-size: 100%;
+}
+
+.bold{font-weight:bold;}
+
+.floatr  {
+  float: right;
+}
+</style>
+
 <?php
 //including the database connection file
 include_once("config.php");
@@ -18,15 +34,15 @@ if(isset($_POST['Submit'])) {
 	if(empty($descricao) || empty($termo)) {
 				
 		if(empty($descricao)) {
-			echo "<font color='red'>descricao field is empty.</font><br/>";
+			echo "<div class='alert alert-danger w-50 p-3' role='alert'>Descricao field is empty.</div>";
 		}
 		
 		if(empty($termo)) {
-			echo "<font color='red'>termo field is empty.</font><br/>";
+			echo "<div class='alert alert-danger w-50 p-3' role='alert'>Termo field is empty.</div>";
 		}
 		
 		//link to the previous pid_termo
-		echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+		echo "</br><a href='javascript:self.history.back();'><button type='button' class='btn btn-secondary'><i class='glyphicon glyphicon-chevron-left'></i>  Go back</button></a>";
 	} else { 
 		// if all the fields are filled (not empty) 
 			
@@ -34,10 +50,20 @@ if(isset($_POST['Submit'])) {
 		$result = mysqli_query($mysqli, "INSERT INTO termos (termo, descricao,id_utilizador) VALUES ('$termo', '$descricao','$id')");
 		
 		//display success messid_termo
-		echo "<font color='green'>Data added successfully.";
-		echo "<br/><a href='index.php'>View Result</a>";
+		echo "<a href='index.php'><button type='button' class='btn btn-secondary'><i class='fa fa-home'></i>  Home</button></a></br></br>";
+		echo "<div class='alert alert-success w-50 p-3' role='alert'>Data added successfully.</div>";
+		
 	}
 }
 ?>
+ <!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  
 </body>
 </html>
